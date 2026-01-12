32e42e66ca0ce9a551cbe2887c05d2bed44b17ff
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-12 20:15:10 +0100

    Convert dependencies to remote URLs

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .library(name: "IEC 61966 2-1", targets: ["IEC 61966 2-1"]),
     ],
     dependencies: [
-        .package(path: "../swift-iso-9899"),
+        .package(url: "https://github.com/swift-standards/swift-iso-9899.git", from: "0.0.1"),
     ],
     targets: [
         // MARK: - Shared
